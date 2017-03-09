0319f1bf7ae5f4229d89cd6ddf35679b09c72ee2
numpad0 <triphenylmethane@gmail.com>
2017-03-09 23:51:53 +0900

    record_dataset.py wasn't working

--- record_dataset.py
+++ record_dataset.py
@@ -8,7 +8,6 @@ import logging
 import queue
 import signal
 import sys
-import queue
 import pygame
 from subprocess import call
 
@@ -41,14 +40,14 @@ def capture_image(filename):
     image_queue.put(image)
 
 def store_image():
-    while(shutdown_signal == False and image_queue.empty == False):
+    while(shutdown_signal == False and image_queue.empty != True):
         image = image_queue.get()
         scipy.misc.imsave("saved_dataset/" + image[0], image[1])
     #cv2.imshow("frame", cv2.cvtColor(image[1], cv2.COLOR_RGB2BGR))
 
 def store_driving_data():
     with open("saved_dataset\dataplus.txt", "a") as dataplus, open("saved_dataset\data.txt", "a") as data:
-        while(input_queue.empty() == False):
+        while(input_queue.empty() !=True):
             vals = input_queue.get()
             print(vals[0], vals[1], vals[2], vals[3], file=dataplus)
             print(vals[0], vals[1],file=data)
@@ -119,11 +118,11 @@ while(True):
         capim.start()
         input_val = (filename, wheel, acc, brake)
         input_queue.put(input_val)
-        if i % 125 == 0:
-            storedata = threading.Thread(target=store_driving_data, name="storedata", args=())
-            storedata.start()
-            storeimage = threading.Thread(target=store_image, name="storeimage", args=())
-            storeimage.start()
+    if i % 125 == 0:
+        storedata = threading.Thread(target=store_driving_data, name="storedata", args=())
+        storedata.start()
+        storeimage = threading.Thread(target=store_image, name="storeimage", args=())
+        storeimage.start()
     clock.tick(25)
     # limits to 25fps
     i += 1
